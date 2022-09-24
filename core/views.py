a6cce3fcb1fefe6090fc72190b335bee533f3887
Phu Nguyen <phutom0412panda@gmail.com>
2022-09-24 12:54:10 -0700

    create watch page with basic function. Have not decorate the page

--- core/views.py
+++ core/views.py
@@ -1,7 +1,9 @@
+from core.models import Movie, Profile
 from django.shortcuts import redirect, render
 from django.views import View
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
+from .forms import ProfileForm
 
 # Create your views here.
 class Home(View):
@@ -19,7 +21,43 @@ class ProfileList(View):
                       'profiles':profile
         })
     
+@method_decorator(login_required, name='dispatch') 
 class ProfileCreate(View):
     def get(self, request, *args, **kargs):
         #form for creating profiles
-        return render(request, 'profileCreate.html')
+        form = ProfileForm
+        return render(request, 'profileCreate.html', {
+            'form':form
+        })
+        
+    def post(self, request, *args, **kargs):
+        form=ProfileForm(request.POST or None)
+        
+        if form.is_valid():
+            #form for creating profiles
+           # print(form.cleaned_data)
+            profile = Profile.objects.create(**form.cleaned_data)
+            if profile:
+                request.user.profiles.add(profile)
+                return redirect('core:profile_list')
+            
+        return render(request, 'profileCreate.html', {
+            'form':form
+        })
+
+@method_decorator(login_required, name='dispatch') 
+class Watch(View):
+    def get(self, request, profile_id, *args, **kargs):
+        try:
+            profile=Profile.objects.get(uuid=profile_id)
+            movies=Movie.objects.filter(age_limit=profile.age_limit)
+            
+            if profile not in request.user.profiles.all():
+                return redirect(to='core:profile_list')
+            
+            return render(request, 'movieList.html', {
+                'movies':movies
+            })
+        except Profile.DoesNotExist:
+            return redirect(to='core:profile_list')
+            
